5f29c4e5f2da0dd64ce4d7d1ed112e345ddc995f
Ninibeth Teixeira <ninibethreina@gmail.com>
2020-01-30 13:55:46 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,15 @@
+class Song 
+  @@all=[]
+  attr_accessor :artist, :genre, :name 
+  
+  def initialize (name,artist,genre)
+    @name=name 
+    @artist=artist
+    @genre=genre 
+    @@all<< self 
+  end 
+  
+  def self.all
+    @@all
+  end 
+end 
